7d5580146970bd6d030329fa4d857eb173f056f7
reckafella <ethanmuthoniw6@gmail.com>
2023-12-08 00:56:04 +0300

    update task 0, 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Module contains a type-annotated function add that takes a float a and a\
     float b as arguments and returns their sum as a float.
