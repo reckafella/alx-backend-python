1094666d7225daa7e1ecd03f7f7fe6ce1c472ce4
reckafella <ethanmuthoniw6@gmail.com>
2023-12-11 19:07:52 +0300

    update task 1 to ensure list is sorted

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,12 +7,12 @@ Module imports wait_random from '0-basic_async_syntax.py'\
 '''
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     function will spawn wait_random n times with the specified max_delay.
     '''
-    wait_random = __import__('0-basic_async_syntax').wait_random
     res = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return res
+    return sorted(res)
