838f32ad63b3f7f094d226057fabfd3de7f0f2fb
reckafella <ethanmuthoniw6@gmail.com>
2023-12-11 18:39:55 +0300

    fix parameter annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,7 +10,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     '''
     see module description above for more details about this async coroutine
     '''
